25452434d6905831e374c85c483edf27f6324fd1
idocohen <ido@build.security>
2021-06-28 11:42:45 +0300

    add tests

---
+++ Tests/OPA-AspDotNetCore-Middleware.Test/RequestProviderTest.cs
@@ -0,0 +1,51 @@
+using ExtensionMethods;
+using FluentAssertions;
+using Xunit;
+
+namespace OPA_AspDotNetCore_Middleware.Test
+{
+    public class Class1Test
+    {
+        [Fact]
+        public void CartesianProduct_twoFullVectors_twoPermissions()
+        {
+            // Arrange
+            var permissions = new string[] { "manager" };
+            var subPermissions = new string[] { "read", "edit" };
+
+            // Act
+            var res = permissions.CartesianProduct(subPermissions);
+        }
+
+        [Fact]
+        public void CartesianProduct_twoEmptyVectors_emptyArray()
+        {
+            // Arrange
+            var permissions = new string[] { };
+            var subPermissions = new string[] { };
+
+            // Act
+            var res = permissions.CartesianProduct(subPermissions);
+
+            // Assert
+            res.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void CartesianProduct_twoLevels_fourPermissions()
+        {
+            // Arrange
+            var permissions = new string[] { "manager", "teacher" };
+            var subPermissions = new string[] { "read", "edit" };
+
+            // Act
+            var res = permissions.CartesianProduct(subPermissions);
+
+            // Assert
+            res.Should().Contain(x => x[0] == "manager" && x[1] == "read");
+            res.Should().Contain(x => x[0] == "manager" && x[1] == "edit");
+            res.Should().Contain(x => x[0] == "teacher" && x[1] == "edit");
+            res.Should().Contain(x => x[0] == "teacher" && x[1] == "edit");
+        }
+    }
+}
